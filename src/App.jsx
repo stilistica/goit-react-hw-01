9877fbd59ef16f3ce14c95934b1b7cf6ecafd404
stillistica <rozhkovavika24@gmail.com>
2025-03-20 15:11:48 +0200

    Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ import TransactionHistory from "./components/transactionHistory/TransactionHisto
 
 
 function App() {
-  return (
-      <>
-          <Profile
-              name={userData.username}
-              tag={userData.tag}
-              location={userData.location}
-              image={userData.avatar}
-              stats={userData.stats}
-          />
-          <FriendList friends={friends} />
-          <TransactionHistory items={transactions} />
-    </>
-	)
+    return (
+        <>
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.avatar}
+                stats={userData.stats}
+            />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </>
+    )
 }
 
 export default App
